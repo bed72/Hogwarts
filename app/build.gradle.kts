2be608943a31e6c9102b053b77263a6fc5823611
bed72 <gjramos1000@gmail.com>
2023-08-05 14:38:53 -0300

    :fire: Configure Style code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 import java.util.Properties
 import java.io.FileInputStream
-
 import kotlin.collections.listOf
 
 plugins {
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,9 @@ buildscript {
 plugins {
     id("com.android.application") version "8.1.0" apply false
 
-    id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-
     id("io.gitlab.arturbosch.detekt") version "1.23.1" apply true
 
+    id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0" apply false
 }
