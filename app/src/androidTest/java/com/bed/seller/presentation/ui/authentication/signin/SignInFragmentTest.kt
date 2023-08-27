d5600a28085c02a8c29ec85fad768cb4d19b27c6
bed72 <gjramos1000@gmail.com>
2023-08-27 13:02:45 -0300

    :fire: Create first ui test with espresso

--- app/src/androidTest/java/com/bed/seller/presentation/ui/authentication/signin/SignInFragmentTest.kt
+++ app/src/androidTest/java/com/bed/seller/presentation/ui/authentication/signin/SignInFragmentTest.kt
@@ -5,18 +5,26 @@ import org.junit.Rule
 import org.junit.Before
 import org.junit.runner.RunWith
 
+import org.hamcrest.Matchers.allOf
+
 import kotlinx.coroutines.runBlocking
 
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 
+import com.google.android.material.snackbar.Snackbar
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 
 import  com.bed.seller.R
 
@@ -39,7 +47,16 @@ internal class SignInFragmentTest {
     }
 
     @Test
-    fun shouldRenderTheSignInFragment(): Unit = runBlocking {
-        onView(withId(R.id.title_text)).check(matches(isDisplayed()))
+    fun shouldRenderTheSignInFragmentWithSuccess(): Unit = runBlocking {
+        onView(withId(R.id.title_text)).check(matches(withText("Bem vindo!")))
+        onView(withId(R.id.subtitle_text)).check(matches(withText("Olá.\nAcesse sua conta e divulge seus produtos")))
+
+        onView(withId(R.id.sign_in_button)).perform(click())
+        onView(
+            allOf(
+                isAssignableFrom(Snackbar.SnackbarLayout::class.java),
+                hasDescendant(withText("Preencha um e-mail válido."))
+            )
+        ).check(matches(isDisplayed()))
     }
 }
