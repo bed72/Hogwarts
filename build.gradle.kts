271ebe116bd891bdcde3393a9af7f042f7da0011
bed72 <gjramos1000@gmail.com>
2023-11-30 15:59:39 -0300

    :fix: Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 plugins {
     id("com.android.application") version "8.1.4" apply false
 
-    id("io.gitlab.arturbosch.detekt") version "1.23.1" apply true
+    id("io.gitlab.arturbosch.detekt") version "1.23.4" apply true
 
     id("org.jetbrains.kotlin.jvm") version "1.9.20" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
