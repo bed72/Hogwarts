65284874c011aa5eaa72103b52927f11734b32f4
bed72 <gjramos1000@gmail.com>
2024-01-13 14:30:42 -0300

    :fire: Adjust layout

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,27 +12,4 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
 }
 
-tasks.register("copyGitHooks", Copy::class.java) {
-    group = "git hooks"
-    into("$rootDir/.git/hooks/")
-    from("$rootDir/scripts/pre-commit")
-    description = "Copies the git hooks from /git-hooks to the .git folder."
-}
-
-tasks.register("installGitHooks", Exec::class.java) {
-    group = "git hooks"
-    workingDir = rootDir
-    commandLine = listOf("chmod")
-    args("-R", "+x", ".git/hooks/")
-    dependsOn("copyGitHooks")
-    description = "Installs the pre-commit git hooks from /git-hooks."
-    doLast {
-        logger.info("Git hook installed successfully.")
-    }
-}
-
-afterEvaluate {
-    tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
-}
-
 true
